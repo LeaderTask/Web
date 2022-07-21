66f380c05bcbd80778484a177ed497524315b15c
Samarkin danya0 Danil <samarkin.danil@yandex.ru>
2022-07-21 14:22:21 +0300

    Добавил в websynk updateTask удаление задачи в случае если удалили дату; и перенос задачи на другую дату

--- src/websync/task.js
+++ src/websync/task.js
@@ -13,6 +13,12 @@ export function removeTask (uid) {
 }
 
 export function updateTask (obj) {
+  // Если меняется дата
+  if (!shouldAddTaskIntoList(obj.obj)) {
+    store.commit('REMOVE_TASK', obj.obj.uid)
+    // Добавление точки на календарь, если дата перенесена, а не удалена
+    store.commit('addDot', new Date(obj.obj.date_begin))
+  }
   store.dispatch('UPDATE_TASK', obj) // updates task and extracts another tags & colors
   if (shouldAddTaskIntoList(obj.obj)) {
     store.commit('ADD_TASK', obj.obj)
